4de4c7a912825dcc8e95fadfc6f0a5338c0faacb
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2023-03-22 20:48:55 +0100

    build.rs that succesfully compiles and links the new C code

---
+++ rsictl/build.rs
@@ -0,0 +1,37 @@
+const INCLUDE_PATHS: [&str; 2] = [
+    "lib/qcbor/inc",
+    "lib/token_verifier",
+];
+
+const SOURCE_FILES: [&str; 7] = [
+    "lib/qcbor/src/ieee754.c",
+    "lib/qcbor/src/qcbor_decode.c",
+    "lib/qcbor/src/qcbor_encode.c",
+    "lib/qcbor/src/qcbor_err_to_str.c",
+    "lib/qcbor/src/UsefulBuf.c",
+    "lib/token_verifier/token_dumper.c",
+    "lib/token_verifier/token_verifier.c",
+];
+
+fn main()
+{
+    // get rid of cross_compile leftover from kernel,
+    // cross compiler is chosen from cargo config
+    std::env::remove_var("CROSS_COMPILE");
+
+    // generic
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}", out_dir);
+
+    // libtoken
+    cc::Build::new()
+        .files(&SOURCE_FILES)
+        .includes(&INCLUDE_PATHS)
+        .compile("libtoken.a");
+
+    for file in SOURCE_FILES {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+
+    println!("cargo:rustc-link-bin=static=token");
+}
